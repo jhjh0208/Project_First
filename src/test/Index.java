b5a8db4517d90350fd8f1b6e7fcc458370b63386
user <chanyoung091@naver.com>
2021-03-24 19:59:44 +0900

    index 03.24~~

---
+++ src/test/Index.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a = 1;
+  int b = 2;
+  int result;
+    result =a + b;
+    System.out.println(result);
+    
+	}
+
+}
