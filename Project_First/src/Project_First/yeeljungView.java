01a6c7a7e1ebb7165bb337adf5d71efd67ef3876
lee-jh0208 <as97164@naver.com>
2021-03-26 18:42:13 +0900

    21.03.26. 로컬에서 수정됨

---
+++ Project_First/src/Project_First/yeeljungView.java
@@ -0,0 +1,22 @@
+package Project_First;
+
+import java.util.Calendar;
+
+import javax.swing.JFrame;
+
+public class yeeljungView extends JFrame{
+	Calendar cl = Calendar.getInstance();
+	
+	
+	public yeeljungView() {
+		initDisplay();
+	}
+	public void initDisplay() {
+	}
+
+	public static void main(String[] args) {
+		new yeeljungView();
+		
+	}
+
+}
