c89c2536871785e0b053bd7b94945eeb245751b0
lee-jh0208 <as97164@naver.com>
2021-03-31 19:51:12 +0900

    하는중

---
+++ Project_First/src/Project_First/yeeljungEvent.java
@@ -0,0 +1,13 @@
+package Project_First;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class yeeljungEvent implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("이거 각각 눌러지나");
+	}
+
+}
--- Project_First/src/Project_First/yeeljungView.java
+++ Project_First/src/Project_First/yeeljungView.java
@@ -3,6 +3,8 @@ package Project_First;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Calendar;
 
 import javax.swing.JButton;
@@ -11,17 +13,22 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class yeeljungView extends JFrame {
-	int			i;
-	Calendar	cl			= Calendar.getInstance();
-	JLabel		jlb			= new JLabel("2021년 " + " 3월");
-	JPanel		jp_up		= new JPanel(new FlowLayout());
-	JPanel		jp_down		= new JPanel(new GridLayout(0, 7, 3, 3));
-	JButton		jbtn_in		= new JButton("출근");
-	JButton		jbtn_out	= new JButton("퇴근");
-	JButton		jbtn_left	= new JButton("◀");
-	JButton		jbtn_right	= new JButton("▶");
-	JButton		jbtn_search	= new JButton("검색");
-	JButton[]	jbtn_nalja	= new JButton[35];
+	int				i;
+	int				j;
+	Calendar		cal			= Calendar.getInstance();
+	JLabel			jlb			= new JLabel("2021년 " + " 3월");
+//	JLabel			jlb2		= new JLabel();
+	JPanel			jp_up		= new JPanel(new FlowLayout());
+	JPanel			jp_up2		= new JPanel(new FlowLayout());
+	JPanel			jp_down		= new JPanel(new GridLayout(0, 7, 3, 3));
+	JButton			jbtn_in		= new JButton("출근");
+	JButton			jbtn_out	= new JButton("퇴근");
+	JButton			jbtn_left	= new JButton("◀");
+	JButton			jbtn_right	= new JButton("▶");
+	JButton			jbtn_search	= new JButton("검색");
+	JButton[]		jbtn_nalja	= new JButton[35];
+//	String[]		str_yooill	= {"일","월","화","수","목","금","토"};
+	yeeljungEvent	yje			= new yeeljungEvent();
 
 	public yeeljungView() {
 		initDisplay();
@@ -37,15 +44,24 @@ public class yeeljungView extends JFrame {
 
 		for (i = 0; i < 35; i++) {
 			jbtn_nalja[i] = new JButton("" + (i + 1));
+			jbtn_nalja[i].addActionListener(yje);
 			jp_down.add(jbtn_nalja[i]);
+
 		}
+		
+//		for (j = 0; j<str_yooill.length;j++) {
+//			jlb2.add(str_yooill[j]);
+//		}
+		
 		this.setLayout(new BorderLayout());
 		this.add("North", jp_up);
+//		this.add("South",jp_up2);
 		this.add("Center", jp_down);
 		this.setTitle("일정관리");
 		this.setSize(650, 500);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 
 	}
 
